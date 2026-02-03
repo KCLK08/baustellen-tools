05232736eaab91634f6edd6a1be573b1ed39f495
Kazim Celik <kazimcelik@MacBookAir.fritz.box>
2026-02-03 21:49:45 +0100

    Update dialogs text

--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.D6XEwfNa.js","_app/immutable/chunks/2.jxRw_DGW.js","_app/immutable/chunks/scheduler.Dlc_SMu7.js","_app/immutable/chunks/index.DCbmexn9.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js"];
-export const stylesheets = ["_app/immutable/assets/2.31HCfVTs.css"];
+export const imports = ["_app/immutable/nodes/2.Ctq6NUu4.js","_app/immutable/chunks/2.DxkZF-k9.js","_app/immutable/chunks/scheduler.Dlc_SMu7.js","_app/immutable/chunks/index.DCbmexn9.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js"];
+export const stylesheets = ["_app/immutable/assets/2.CZrOl_SY.css"];
 export const fonts = [];
